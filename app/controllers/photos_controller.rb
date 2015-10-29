ad9843597b115e255e7fd86fde5abef0d5b377c8
mloayzat <mloayzat@chicagobooth.edu>
2015-10-28 20:25:42 -0500

    Finished

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,51 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+
+  	@photo = Photo.find_by({:id => params[:id] })
+  	
+  end
+
+  def new_form
+  end
+
+  def delete_row
+  	p = Photo.find_by({:id => params[:id] })
+  	p.destroy
+
+  	redirect_to("http://localhost:3000/photos")
+  end
+
+  def create_row
+  	p = Photo.new
+  	p.caption = params[:the_caption]
+  	p.source = params[:the_source]
+  	p.save
+
+  	redirect_to("http://localhost:3000/photos")
+  end
+
+  def edit_form
+  	@photo = Photo.find_by({:id => params[:id] })
+  	x = Photo.find_by({:id => params[:id] })
+  	@old_caption = x.caption
+  	@old_source = x.source 
+
+
+  end
+
+  def edit_row
+  	@photo = Photo.find_by({:id => params[:id] })
+  	p = Photo.find_by({:id => params[:id] })
+  	p.caption = params[:new_caption]
+  	p.source = params[:new_source]
+
+  	p.save
+
+  	redirect_to("http://localhost:3000/photos/"+@photo.id.to_s)
+
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,16 @@ Rails.application.routes.draw do
 
   get("/", { :controller => "photos", :action => "index" })
 
+  get("/photos/new",           		{ :controller => "photos", :action => "new_form" })
+  get("/create_photo",           	{ :controller => "photos", :action => "create_row" })
+  get("/delete_photo/:id",  		{ :controller => "photos", :action => "delete_row" })
+  get("/edit_photo/:id",  			{ :controller => "photos", :action => "edit_row" })
+
+
   # Routes to READ photos
-  get("/photos",           { :controller => "photos", :action => "index" })
-  get("/photos/:id",       { :controller => "photos", :action => "show" })
+  get("/photos",           			{ :controller => "photos", :action => "index" })
+  get("/photos/:id",       			{ :controller => "photos", :action => "show" })
+  get("/photos/:id/edit",  			{ :controller => "photos", :action => "edit_form" })
+ 
 
 end
